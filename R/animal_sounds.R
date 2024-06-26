83394d34b10b00d754819a2bc1f15c37de8406d0
glen <glen.guyver-fletcher@warwick.ac.uk>
2024-06-26 13:05:37 +0100

    Roxygen documentation

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,5 +1,16 @@
+#' Print an animal with provided sound
+#'
+#' @param animal An animal name, e.g. "Tiger", "Dog"
+#' @param sound A sound the animal will make, e.g. "Roar", "Woof"
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog", "woof")
 animal_sounds <- function(animal, sound) {
+
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
-  paste0("The ", animal, " says ", sound, "!")
+  paste0("The ", animal, " says ", sound, ", ", sound, ", ", sound, "!")
 }
